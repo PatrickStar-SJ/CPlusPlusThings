943684562a8f026162e7e3566ebfce10f4fc4994
sunjun <18810256350@163.com>
2023-08-01 10:49:53 +0800

    update

--- cpp2.0/cpp11/default_delete.cpp
+++ cpp2.0/cpp11/default_delete.cpp
@@ -92,7 +92,7 @@ class Foo : public PrivateCopy {
 int main() {
     Zoo z;
     Zoo z1(1, 2);
-//    Zoo z2=z;   // error copy ctor delete
+//    Zoo z2=z;   // 禁止使用复制构造函数来创建Zoo对象的副本。
     z = z1;
     complex<int> a;     // 内部不带指针的可以不用写 big three 没有析构 有copy ctor与copy assignmetn 都是使用=default,没有自己写
     string s;       // 内部带指针的有big five
@@ -105,3 +105,39 @@ int main() {
 //    foo = foo1;   // 继承了的都拥有PrivateCopy性质
     return 0;
 }
+
+/*
+class Zoo:
+    上述代码定义了一个名为Zoo的类。该类有两个私有成员变量a和b，表示动物园的某些属性。
+
+    Zoo(int a, int b) : a(a), b(b)是一个带有参数的构造函数。当创建Zoo对象时，可以传入两个整数参数a和b来初始化成员变量a和b。构造函数中的cout语句用于输出一条带有参数的构造函数的信息。
+
+    Zoo() = default;是一个默认构造函数。当创建Zoo对象时，如果没有传入任何参数，则会调用该默认构造函数进行初始化。
+
+    Zoo(const Zoo &) = delete;是一个删除的复制构造函数。这意味着在代码中禁止使用复制构造函数来创建Zoo对象的副本。
+
+    Zoo(Zoo &&) = default;是一个移动构造函数。它允许使用移动语义来创建Zoo对象的副本。
+
+    Zoo &operator=(const Zoo &) = default;是一个默认的复制赋值运算符。它允许将一个Zoo对象的值复制给另一个Zoo对象。
+
+    Zoo &operator=(const Zoo &&) = delete;是一个删除的移动赋值运算符。这意味着在代码中禁止使用移动赋值运算符来将一个Zoo对象的值移动给另一个Zoo对象。
+
+    void fun1() = delete;是一个被删除的成员函数。这意味着在代码中禁止使用名为fun1的函数。
+
+    ~Zoo() = default;是一个默认析构函数。它用于在Zoo对象被销毁时执行清理操作。
+
+    总结：
+    = default可以用于构造函数（包括默认构造函数、复制构造函数和移动构造函数）、析构函数和复制赋值运算符，表示使用编译器生成的默认实现。
+    = delete可以用于复制构造函数、移动赋值运算符和成员函数，表示禁用对应的函数或者操作。
+
+
+class PrivateCopy：
+    PrivateCopy(const PrivateCopy &) {};：这是一个私有的拷贝构造函数（Copy Constructor），
+    它通过将其参数声明为对PrivateCopy类的引用来禁止对象的拷贝构造。这意味着无法使用拷贝构造函数创建该类的新对象。
+
+    PrivateCopy &operator=(const PrivateCopy &) {};：这是一个私有的赋值运算符重载函数，
+    它通过将其参数声明为对PrivateCopy类的引用来禁止对象之间的赋值操作。这意味着无法使用赋值运算符将一个PrivateCopy对象的值复制到另一个PrivateCopy对象中。
+
+    该类的目的是禁止对象的拷贝和赋值操作。通过将拷贝构造函数和赋值运算符重载函数声明为私有，并且不提供实现，
+    可以防止其他代码对该类进行拷贝或赋值操作。只允许通过默认构造函数创建对象，并在对象销毁时执行一些清理操作。
+*/
--- cpp2.0/cpp11/template_template.cpp
+++ cpp2.0/cpp11/template_template.cpp
@@ -6,8 +6,7 @@
 
 using namespace std;
 // template template parameter 模板模板参数
-template<typename T,
-        template<typename U> class Container>
+template<typename T,template<typename U> class Container>
 class XCls {
 private:
     Container<T> c;
@@ -23,3 +22,21 @@ int main() {
 
     return 0;
 }
+/*
+以上代码是一个使用模板的C++类定义和使用示例。
+
+首先，template<typename T, template<typename U> class Container> 表示定义一个模板类 XCls，它有两个模板参数：T 和 Container。
+
+T 是一个通用类型参数，表示 XCls 中要存储的元素的类型。
+Container 是一个模板模板参数，表示 XCls 中使用的容器类型。这里的容器类型是一个模板，它接受一个类型参数 U。
+在 XCls 类中，私有成员变量 c 的类型是 Container<T>，即根据模板参数确定的具体容器类型 Container，其中元素类型为 T。这样，每个 XCls 对象都会包含一个特定类型的容器。
+
+接下来，通过别名模板 using Lst = list<T>; 定义了一个 Lst，它是 list 类型的别名，其中元素类型为 T。
+
+在 main 函数中，注释掉的 XCls<string, list> mylist1; 会导致编译错误，
+原因是list是一个类模板，而XCls的第二个模板参数要求是一个模板模板参数，它需要接受一个类型参数，因此list不能直接作为XCls的模板参数。
+
+然而，XCls<string, Lst> mylist2; 使用了别名模板 Lst，将 list 作为模板参数传递给 XCls 类模板，创建了一个 XCls 对象 mylist2，其中的容器类型为 list<string>。
+
+最后，return 0; 结束 main 函数，并返回 0 表示程序成功执行完毕。
+*/
